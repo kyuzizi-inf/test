340aaabfc4823b96bf927c6dd60eecf9ecdff5c9
kyuzizi-inf <84620199+kyuzizi-inf@users.noreply.github.com>
2024-07-19 15:38:48 +0800

    Create flow.py

---
+++ flow.py
@@ -0,0 +1,21 @@
+from prefect import flow
+import random
+
+def add_int(a, b):
+    print("%d 加 %d 是 %d"%(a, b, a + b))
+
+@flow(log_prints=True)
+def sum_up_task():
+    i = 0
+    while(i <= 10):
+        i += 1 
+        add_int(random.randint(0,100), random.randint(0,50))
+
+if __name__ == "__main__":
+    sum_up_task.from_source(
+            "http://gitlab.alibaba-inc.com/chenzhangxinyu.czx/wp-test.git",
+            entrypoint="flow.py:sum_up_task",
+    ). deploy(
+        name = "simple-work-pool-test",
+        work_pool_name = "developing-work-pool",
+    )
